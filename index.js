d8c5d80965b2794b611aea36684bac9d0ec33f2d
raymond_cty <cty0613@hanmail.net>
2024-05-30 01:41:27 +0900

    Feat: Milestone Step

---
+++ index.js
@@ -0,0 +1,25 @@
+const TAG = "[index.js]"
+const CAT_EVENT = "[EVENT]"
+
+function indexFirst(char){
+
+}
+
+$(".book-item-dsc").hover(function (e) {
+        var bookId = $(e.currentTarget).data("idx");
+        var portraitPath = "src/portrait/pt_" + bookId + ".png"
+        // var currentIndicate = $(e.currentTarget).find(".book-item-indicator")
+        // var allIndicate = $('.book-table').find(".book-item-indicator")
+        // console.log(TAG, CAT_EVENT, portraitPath)
+        $(".book-item-portrait").attr("src", portraitPath);
+        
+        $('.book-table').find(".book-item-indicator").removeClass("hoverd")
+        $(e.currentTarget).find(".book-item-indicator").addClass("hoverd")
+    }
+);
+
+$(".book-item-dsc").click(function (e) {
+    var bookId = $(e.currentTarget).data("idx");
+    sessionStorage.getItem("selectedVOL", bookId)
+});
+
